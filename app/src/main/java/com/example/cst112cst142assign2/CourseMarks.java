4a490560c3eb3673c909564df894574698487601
Simon Gilmour <Gilmour9639@saskpolytech.ca>
2019-05-17 14:43:08 -0600

    "Table creation"

---
+++ app/src/main/java/com/example/cst112cst142assign2/CourseMarks.java
@@ -0,0 +1,19 @@
+package com.example.cst112cst142assign2;
+
+public class CourseMarks {
+
+    public double mark;
+    public double weight;
+    public String CourseCode;
+    public String CourseMarks;
+
+
+
+    public CourseMarks(String CourseMarks, String CourseCode, double weight, double mark)
+    {
+        this,CourseMarks = CourseMarks
+        this.CourseCode = CourseCode;
+        this.weight = weight;
+        this.mark = mark;
+    }
+}
---
+++ app/src/main/java/com/example/cst112cst142assign2/CourseMarksHrelper.java
@@ -0,0 +1,15 @@
+package com.example.cst112cst142assign2;
+
+public class CourseMarksHrelper {
+
+    public static final String DB_NAME = "Courses.db";
+    public static final String TABLE_NAME = "CourseMarks";
+    public static final int DB_VERSION = 1;
+    public static final String ID = "_id";
+    public static final String MARK = "mark";
+    public static final String WEIGHT = "weight";
+    public static final String COURSEMARK = "coursemark";
+    public static final String COURSECODE = "coursecode";
+
+
+}
