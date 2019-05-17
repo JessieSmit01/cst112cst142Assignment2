10ae75bf1f0a038a9575ff0acc1aa613881fe627
Jessie Smith <smith8421@saskpolytech.ca>
2019-05-17 14:39:03 -0600

    Added Course object

---
+++ app/src/main/java/com/example/cst112cst142assign2/Course.java
@@ -0,0 +1,15 @@
+package com.example.cst112cst142assign2;
+
+public class Course {
+
+    public String courseCode;
+    public String sName;
+    public String sYear;
+
+    public Course(String courseCode, String sName, String sYear)
+    {
+        this.courseCode = courseCode;
+        this.sName = sName;
+        this.sYear = sYear;
+    }
+}
